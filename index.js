887757ecc81a9ed3b56e5e85a8d277a63251290f
Daniel Muthui <dannymuthui118@gmail.com>
2026-04-02 10:55:08 +0300

    feat: all about scoping

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+let milkshakeMenu = ["vanilla","chocolate"];
+let burgerMenu = ["cheeseburger","veggie burger"];
+
+function addMilkshake(item){
+    let newMilkshake = item;
+    milkshakeMenu.push(newMilkshake);
+    return milkshakeMenu;
+}
+
+function addBurger(item){
+    let newBurger = item;
+    burgerMenu.push(newBurger);
+    return burgerMenu;
+}
+function showMenu(){
+    let fullMenu = [];
+    for (let i = 0; i < milkshakeMenu.length; i++) {
+    fullMenu.push(milkshakeMenu[i] + " + " + burgerMenu[i % burgerMenu.length]);
+  } 
+  return fullMenu;
+}
+console.log(addMilkshake("Strawberry"));
+console.log(addBurger("Bacon Burger"));
+console.log(showMenu());
